93f8ae449cef3b0919f3caf8e494de4087ade3e1
Dinesh babu <dbramanayanam@gmail.com>
2025-05-03 14:15:20 +0400

    added files

--- vpc/variable.tf
+++ vpc/variable.tf
@@ -2,3 +2,13 @@ variable "tags_environment" {
   type    = string
   default = "dev"
 }
+
+variable "project" {
+  type    = string
+  default = "roboshop"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
