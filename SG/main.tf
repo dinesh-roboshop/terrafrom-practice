108b8b05dbb8a2aad0fc803bfc2c8afc93421e78
Dinesh babu <dbramanayanam@gmail.com>
2025-05-03 15:01:29 +0400

    added files

--- SG/main.tf
+++ SG/main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_all_ipv4" {
   name        = "allow-all-ipv4"
   description = "Allow all inbound and outbound IPv4 traffic"
-  vpc_id      = data.aws_ssm_parameter.vpc_id.Value
+  vpc_id      = data.aws_ssm_parameter.vpc_id.value
   ingress {
     from_port   = 0
     to_port     = 0
