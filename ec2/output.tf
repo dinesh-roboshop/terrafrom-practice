9547b4728d4866837b1b4530461c5b58255a2bcf
Dinesh babu <dbramanayanam@gmail.com>
2025-05-03 22:15:35 +0400

    added files

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,14 +1,5 @@
-output "vpc_id" {
-  value       = module.vpc.vpc_id
-  description = "List of all VPC ids"
+output "ec2_id" {
+  value       = module.ec2_instance.id
+  description = "ec2 id"
 
 }
-output "public_subnets" {
- value = module.vpc.public_subnets
- description = "Public subnets"
-}  
-
-output "private_subnets" {
- value = module.vpc.private_subnets
- description = "Private subnets"
-}  
