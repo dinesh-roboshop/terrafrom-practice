3aab5f0ff220ae04b0c6197a5a07fad7ec08a36f
Dinesh babu <dbramanayanam@gmail.com>
2025-05-03 15:04:55 +0400

    added files

--- SG/output.tf
+++ SG/output.tf
@@ -1,5 +1,5 @@
 output "sg_id" {
-  value       = aws_security_group.allow_all_ipv4.security_group_id
+  value       = aws_security_group.allow_all_ipv4.id
   description = "List of all SG ids"
 
 }
