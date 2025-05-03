1ec5d827721f56e93cf3e1cd695b695dd36d6032
Dinesh babu <dbramanayanam@gmail.com>
2025-05-03 14:03:25 +0400

    added files

--- vpc/output.tf
+++ vpc/output.tf
@@ -3,3 +3,12 @@ output "vpc_id" {
   description = "List of all VPC ids"
 
 }
+output "public_subnets" {
+ value = module.vpc.public_subnets
+ description = "Public subnets"
+}  
+
+output "private_subnets" {
+ value = module.vpc.private_subnets
+ description = "Private subnets"
+}  
