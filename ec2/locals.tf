deef33826706d594f32aa48e40b6fae2c371d6e2
Dinesh babu <dbramanayanam@gmail.com>
2025-05-03 22:18:30 +0400

    added files

--- ec2/locals.tf
+++ ec2/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  public_subet_id = element (split(",", data.aws_ssm_parameter.public_subnet_id.value), 0)
+  public_subet_id = element(split(",", data.aws_ssm_parameter.public_subnet_id.value), 0)
 }
