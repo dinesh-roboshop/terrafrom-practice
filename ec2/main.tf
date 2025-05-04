35678e37f672791752a12b10487075fb7b44e314
Dinesh babu <dbramanayanam@gmail.com>
2025-05-04 12:23:10 +0400

    added files

--- ec2/main.tf
+++ ec2/main.tf
@@ -10,7 +10,7 @@ module "ec2_instance" {
   vpc_security_group_ids = [data.aws_ssm_parameter.sg_id.value]
   subnet_id              = local.public_subet_id
   associate_public_ip_address = true
-  user_data               = file("${path.module}/ec2-provision.sh")
+  user_data               = file("ec2-provision.sh")
 
 tags = var.common_tags
 
